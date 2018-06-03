eb1cdeda9fb82884f4a88087002a528078c95f31
Raja <>
2018-06-03 11:29:56 +0530

    updated

--- app/src/main/java/com/technorabit/agritech/FarmerListActivity.java
+++ app/src/main/java/com/technorabit/agritech/FarmerListActivity.java
@@ -23,6 +23,16 @@ public class FarmerListActivity extends BaseActivity {
 
     private void initGUI() {
 
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                //start activity code
+                startActivity(FarmerListActivity.this,NewFarmerActivity.class,false);
+
+            }
+        });
     }
 
 }
