82d6a4c330336bcd3a564cc89f492da60b9d5bdb
Raja <raja.peela@capillarytech.com>
2018-06-03 11:08:12 +0530

    updated

--- app/src/main/java/com/technorabit/agritech/FarmerListActivity.java
+++ app/src/main/java/com/technorabit/agritech/FarmerListActivity.java
@@ -7,7 +7,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 
-public class FarmerListActivity extends AppCompatActivity {
+public class FarmerListActivity extends BaseActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,15 +15,14 @@ public class FarmerListActivity extends AppCompatActivity {
         setContentView(R.layout.activity_farmer_list);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setTitle("Formers");
+        initGUI();
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+    }
+
+    private void initGUI() {
+
     }
 
 }
---
+++ app/src/main/java/com/technorabit/agritech/model/FarmerRes.java
@@ -0,0 +1,17 @@
+package com.technorabit.agritech.model;
+
+import java.util.ArrayList;
+
+/**
+ * Created by raja on 03/06/18.
+ */
+
+public class FarmerRes {
+
+    public ArrayList<Farmer> Content;
+
+    public static class Farmer {
+        String firstName, middleName, lastName, phNumber, districtId, districtName,
+                mandalId, mandalName, villageId, villageName, seasonName, landArea, cropDetails, remarks;
+    }
+}
